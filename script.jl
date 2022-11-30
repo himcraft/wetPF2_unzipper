5dd7c81bac0a72add59399477476ed6a21560e68
himcraft <1014harry@gmail.com>
2022-11-30 23:41:19 +0800

    added script

---
+++ script.jl
@@ -0,0 +1,15 @@
+using Dates
+#=====
+unzip all found wetPf2 files in certain directory
+=====#
+cd("I:/CDAAC/COSMIC/Update/")
+result=filter(isfile,readdir())
+# "wetPf2_nrt_yyyy_ddd.tar.gz"
+for (i,name) in enumerate(result)
+    if(length(name)<10 || name[1:10]!="wetPf2_nrt")
+        continue
+    end
+    fnm=name[12:15]*"."*name[17:19]
+    cmd=`tar -zxvf $name -C ./$fnm`
+    run(cmd)
+end
