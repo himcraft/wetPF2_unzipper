8eb97c44bf33166514db8416b583b2edc4dd2177
himcraft <1014harry@gmail.com>
2022-12-01 00:07:48 +0800

    minor update

--- script.jl
+++ script.jl
@@ -1,11 +1,10 @@
-using Dates
 #=====
 unzip all found wetPf2 files in certain directory
 =====#
 cd("I:/CDAAC/COSMIC/Update/")
 result=filter(isfile,readdir())
 # "wetPf2_nrt_yyyy_ddd.tar.gz"
-for (i,name) in enumerate(result)
+for name in result
     if(length(name)<10 || name[1:10]!="wetPf2_nrt")
         continue
     end
